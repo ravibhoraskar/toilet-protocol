cf9f5ea388adbb062b03c5d47f932773ac3bbb01
Ravi Bhoraskar <rav@fb.com>
2022-12-10 14:54:45 -0800

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, ReactNode } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ToiletState from "./ToiletState";
+import Toilet from "./Toilet";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      toiletState: ToiletState.Up,
+      lastPerson: null,
+      lastUsage: null,
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  render(): ReactNode {
+    return (
+      <div className="App">
+        <center>
+          <button className="GenerateButton" onClick={this.onClick}>
+            Next!
+          </button>
+          <Toilet toilet={this.state.toiletState}/>
+        </center>
+      </div>
+    );
+  }
+
+  onClick() {}
 }
-
 export default App;
